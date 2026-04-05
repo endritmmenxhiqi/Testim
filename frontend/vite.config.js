093ae97c1a055ac3f3167280a4583a5807db2f77
endritmmenxhiqi <endrit.menxhiqi123@gmail.com>
2026-04-05 11:00:24 +0200

    Rregullimi i 404 dhe vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Kjo shton pikën para rrugëve të skedarëve që të gjenden pas build-it
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: 'dist',
   }
 })
